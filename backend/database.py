449a96213f499757e871daeb42191e4708716aa7
Gaudy035 <miloszgaudy@gmail.com>
2026-03-22 17:20:05 +0100

    .env dodany

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DB_USER = 'fin_calc_api'
-DB_PASS = 'fin_calc_pass'
-DB_HOST = 'localhost'
-DB_PORT = '5432'
-DB_NAME = 'fin_calc_db'
+load_dotenv()
+
+# DB_USER = 'fin_calc_api'
+# DB_PASS = 'fin_calc_pass'
+# DB_HOST = 'localhost'
+# DB_PORT = '5432'
+# DB_NAME = 'fin_calc_db'
+
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", '5432')
+DB_NAME = os.getenv("DB_NAME")
 
 DB_PATH = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
